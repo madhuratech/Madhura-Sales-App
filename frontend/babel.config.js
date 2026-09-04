943597b2f94f4bd456fdda0b31e905bc95718409
arounesharoulor <arounesharoulor@gmail.com>
2026-09-04 21:16:39 +0530

    .....

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -2,8 +2,12 @@ module.exports = function (api) {
   api.cache(true);
   const plugins = [];
   
+  // Robust check to see if we are building for web (Vercel)
+  const isWeb = api.caller(caller => (caller && caller.platform === 'web') || (caller && caller.name === 'babel-loader'));
+  const isExpoOSWeb = process.env.EXPO_OS === 'web';
+  
   // Only apply Reanimated plugin for native builds to prevent Web build (Vercel) crash
-  if (process.env.EXPO_OS !== 'web') {
+  if (!isWeb && !isExpoOSWeb) {
     plugins.push('react-native-reanimated/plugin');
   }
 
