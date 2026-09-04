7fe7d3cc3d7c4ec69fe5dde6ecbb9fff2bcdfd79
arounesharoulor <arounesharoulor@gmail.com>
2026-09-04 21:05:33 +0530

    ...

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,7 +1,12 @@
 module.exports = function (api) {
   api.cache(true);
-  const plugins = ['react-native-reanimated/plugin'];
+  const plugins = [];
   
+  // Only apply Reanimated plugin for native builds to prevent Web build (Vercel) crash
+  if (process.env.EXPO_OS !== 'web') {
+    plugins.push('react-native-reanimated/plugin');
+  }
+
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
