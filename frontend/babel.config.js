a47dec5a7564a3b6434347d14e30f2435597a345
arounesharoulor <arounesharoulor@gmail.com>
2026-09-04 19:26:23 +0530

    update apk

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,12 +1,7 @@
 module.exports = function (api) {
-  const isWeb = api.caller((caller) => caller && (caller.name === 'metro' ? false : caller.name === 'babel-loader'));
-  
-  const plugins = [];
-  if (process.env.EXPO_OS !== 'web' && !isWeb) {
-    plugins.push('react-native-reanimated/plugin');
-  }
-
   api.cache(true);
+  const plugins = [];
+  
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
